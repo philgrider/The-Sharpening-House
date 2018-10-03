14bd5ac7ea4af93b2e0e1bb4a9b68d99ddde61fc
kle9ej <kevin_luu@homedepot.com>
2018-10-03 19:27:34 -0400

    fixed formatting

--- assets/javascript/twilio.js
+++ assets/javascript/twilio.js
@@ -1,11 +1,15 @@
 $("#send-text").click(function() {
 
+    // Twilio config
     var SID = 'ACf8043b474d2bea343b097a8ace8c04c5';
     var Key = '4dcb3ca4a8529b471e755f97c9c4f1a9'
     var Auth =  btoa(SID + ':' + Key);
     var twilio_url = 'https://api.twilio.com/2010-04-01/Accounts/' + SID + '/Messages.json';
+    var fromNumber = '+14702644689'
+    var toNumber = ''
     var body = '';
 
+    // AJAX call to Twilio to send SMS
     $.ajax({
         url: twilio_url,
         type: 'POST',
@@ -13,13 +17,15 @@ $("#send-text").click(function() {
             "Authorization": "Basic " + Auth
         },
         data: {
-            From: '+14702644689', 
+            From: fromNumber, 
             Body: body, 
-            To: '+16787990551'
+            To: phoneNumber
         },
+        // Log errors
         error: function(err) {
             console.log('Error!', err);
         },
+        // Log success
         success: function(success) {
             console.log('Success!');
         }

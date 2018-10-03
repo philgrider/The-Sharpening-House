1721f9f428579a2d33e7cd5735428c69428a5551
PHILGRIDER <39570510+philgrider@users.noreply.github.com>
2018-10-02 21:54:51 -0400

    added both functions into one file

--- call-recipes-file.js
+++ call-recipes-file.js
@@ -14,3 +14,18 @@ function getRecipes(foodCatagory, callback) {
   });
 
 };
+function getRecipe(recipieID, callback) {
+
+  var queryURL = "./First-Recipe.JSON";
+  
+  $.getJSON( queryURL, function ( response ) {
+    // console.log('type of response', typeof response);
+    // console.log('Recipe',JSON.stringify(response));
+  
+    if (typeof response === 'string') {
+      response = JSON.parse(response);
+    }
+  
+    callback(response);
+  });
+  };

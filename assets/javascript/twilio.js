1245cca03baef3328d4b0435ffd5b7ba4968fa8e
kle9ej <kevin_luu@homedepot.com>
2018-10-03 20:44:25 -0400

    create function to take userNumber and bodyMessage params

--- assets/javascript/twilio.js
+++ assets/javascript/twilio.js
@@ -1,34 +1,42 @@
-$("#send-text").click(function() {
+function sendSMS(userNumber, bodyMessage) {
+
+    // Store parameters in local variables
+    var toNumber = userNumber;
+    var body = bodyMessage;
 
     // Twilio config
-    var SID = 'ACf8043b474d2bea343b097a8ace8c04c5';
-    var Key = '4dcb3ca4a8529b471e755f97c9c4f1a9'
-    var Auth =  btoa(SID + ':' + Key);
-    var twilio_url = 'https://api.twilio.com/2010-04-01/Accounts/' + SID + '/Messages.json';
-    var fromNumber = '+14702644689'
-    var toNumber = ''
-    var body = '';
+    var sid = 'ACf8043b474d2bea343b097a8ace8c04c5';
+    var key = '4dcb3ca4a8529b471e755f97c9c4f1a9';
+    var auth =  btoa(sid + ':' + key);
+    var twilio_url = 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json';
+    var fromNumber = '+14702644689';
+   
 
     // AJAX call to Twilio to send SMS
     $.ajax({
         url: twilio_url,
         type: 'POST',
         headers: {
-            "Authorization": "Basic " + Auth
+            "Authorization": "Basic " + auth
         },
         data: {
             From: fromNumber, 
             Body: body, 
-            To: phoneNumber
+            To: toNumber
         },
         // Log errors
         error: function(err) {
-            console.log('Error!', err);
+            console.log('Error: ', err);
         },
         // Log success
         success: function(success) {
-            console.log('Success!');
+            console.log('SMS Sent Successfully!');
         }
     });
 
-});
+}
+
+
+
+
+

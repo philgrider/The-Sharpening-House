24f903239fc8b56254640739ed95d2b52c824626
kle9ej <kevin_luu@homedepot.com>
2018-10-06 14:30:53 -0400

    add user data into firebase

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     var $userPass = $('#password');
     var $submitBtn = $('#submit-signup-btn');
     var $errorMsg = $('#error-message');
+    var $phoneNumber = $('#phone-number');
 
     // Create on click event listener for submit button
     $submitBtn.on('click', function(event) {
@@ -18,15 +19,31 @@ $(document).ready(function() {
         // Store user input into local variables
         var email = $userEmail.val();
         var pass = $userPass.val();
+        var phone = $phoneNumber.val();
 
         // Check if password is at least 8 characters long
         if (pass.length < 7) {
             $errorMsg.text('*Password must be at least 8 characters long!');
             $userPass.val('');
         } else {
-            //Create user, then redirect to home page, or catch error
+
+            // Create user, then redirect to index.html, or catch error
             firebase.auth().createUserWithEmailAndPassword(email, pass).then(function() {
-                window.location = "index.html";
+                
+                // Get current userid from firebase and store in local variable
+                var userId = firebase.auth().currentUser.uid;
+
+                // Store user data into firebase
+                firebase.database().ref('users/' + userId).set({
+                    userid: userId,
+                    email: email,
+                    phoneNumber : phone
+                }).then(function() {
+                    // Relocate to index.html after user is created
+                    window.location = "index.html";
+                });
+                
+            // Catch error and console the log it
             }).catch(function(err) {
                 console.log(err.message);
             });

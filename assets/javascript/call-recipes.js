ef1495f7133ccd8794a474ea3ac0754efe4e3041
PHILGRIDER <39570510+philgrider@users.noreply.github.com>
2018-10-06 08:28:17 -0500

    added ingredients pop up

--- assets/javascript/call-recipes.js
+++ assets/javascript/call-recipes.js
@@ -1,7 +1,8 @@
 
 function getRecipes(foodCatagory, callback) {
-
-  var queryURL = "https://www.food2fork.com/api/search?key=890f6487752c619e7b9ad1ca807eeff8&q=" + foodCatagory.toLowerCase();
+  var vitaApiKey = 'd22669e556f9e536d2d4e73b6cb07813';
+  // var philApiKey = '890f6487752c619e7b9ad1ca807eeff8';
+  var queryURL = 'https://www.food2fork.com/api/search?key=' + vitaApiKey + '&q=' + foodCatagory.toLowerCase();
 
   $.ajax({
     url: queryURL,
--- assets/javascript/queryRecipes.js
+++ assets/javascript/queryRecipes.js
@@ -13,6 +13,7 @@ getRecipes($recipeType,updateDOM);
 });
 function updateDOM (recipes) {
     recipeDataStore = JSON.parse(recipes);
+    $recipesDisplay.empty();
     console.log('Chicken Recipies', (recipeDataStore));
     var $headerResults = $('<h4>')
         .addClass('text-center border-bottom')
@@ -41,15 +42,37 @@ function updateDOM (recipes) {
         });
         var $cardBodyContainer = $('<div>').addClass('card-body');
         var $pCard = $('<p>').addClass('card-text text-center').text(recipeDataStore.recipes[i].title);
+        var $recipeDrop = $('<div>').addClass('drop-container');
+        $recipeDrop.append($('<span>').addClass('button').attr({'id':'drop-button','data-recipe-id': recipeDataStore.recipes[i].recipe_id}).text('+'));
+        var $ingredientsDiv = $('<div>').addClass('drop-content');
+        $ingredientsDiv.append($('<div>').addClass('drop-head').html('<h3>' + recipeDataStore.recipes[i].title + '</h3>'));
+        var $dropBody = $('<div>').addClass('drop-body');
+        $dropBody.append($('<div>').attr({
+                    class: 'list-group',
+                    'data-spy': 'scroll',
+                    'data-id': 'ingredients-area',
+                    'data-offset': '0'
+        }));
+        $ingredientsDiv.append($dropBody);
+        $recipeDrop.append($ingredientsDiv);
         $cardBodyContainer.append($pCard);
-        $cardDiv.append($cardImg, $cardBodyContainer);
+        $cardDiv.append($cardImg, $cardBodyContainer, $recipeDrop);
         $recipesDisplay.append($cardDiv);
     }
 
 
   }
-  function updateDOM2 (recipe) {
+  $('#recipe-query-results').unbind('click').on('click', '.button',function (e) {
+    e.preventDefault();
+    $(this).parent().toggleClass('expand');
+    $(this).parent().children().toggleClass('expand');
+    var recipeId = $(this).attr('data-recipe-id');
+    console.log('This should be the Recipe ID' + recipeId);
+    getRecipe(recipeId, ingredients);
+  });
+  function ingredients (recipe) {
      console.log('recipie', (recipe));
+
       // recipes = the JSON you returned in call-recipe.js
   };
 //   getRecipe('35120', updateDOM2);
